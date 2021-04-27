800e8d4fcaf2335238ec329d295b7cdde4b778c4
Parkhomenko Egor <54312766+1MPULSEONE@users.noreply.github.com>
2021-04-27 13:28:27 +0600

    Added check if user has been registered
    Signed-off-by: “Parkhomenko <parkhomenko2049@gmail.com>

--- app/src/main/java/com/ninjapath/besteducation/activities/GreetingActivity.java
+++ app/src/main/java/com/ninjapath/besteducation/activities/GreetingActivity.java
@@ -6,12 +6,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.firebase.ui.auth.viewmodel.AuthViewModelBase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.ninjapath.besteducation.R;
 
 public class GreetingActivity extends AppCompatActivity {
 
     private Button signIn;
     private Button logIn;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Intent intentToHome = new Intent(GreetingActivity.this,
+                    MainActivity.class);
+            startActivity(intentToHome);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
