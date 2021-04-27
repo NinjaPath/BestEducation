304666b9e1d2a8a85bf6f76388a004029a0e81b3
“Parkhomenko <parkhomenko2049@gmail.com>
2021-04-27 15:19:04 +0600

    Small changes

--- app/src/main/java/com/ninjapath/besteducation/activities/MainActivity.java
+++ app/src/main/java/com/ninjapath/besteducation/activities/MainActivity.java
@@ -1,9 +1,16 @@
 package com.ninjapath.besteducation.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.ninjapath.besteducation.ChatsFragment;
+import com.ninjapath.besteducation.HomeFragment;
+import com.ninjapath.besteducation.ProfileFragment;
 import com.ninjapath.besteducation.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,5 +19,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            item -> {
+                Fragment selectedFragment = null;
+                switch (item.getItemId()){
+                    case  R.id.nav_home:
+                        selectedFragment = new HomeFragment();
+                        break;
+                    case  R.id.nav_chats:
+                        selectedFragment = new ChatsFragment();
+                        break;
+                    case  R.id.nav_profile:
+                        selectedFragment = new ProfileFragment();
+                        break;
+
+                }
+
+                assert selectedFragment != null;
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        selectedFragment).commit();
+                return true;
+
+            };
+
+
 }
