3fa7034d253354004923e57a273eacae09fee3d8
OlegSadJktu <plyuha2004@mail.ru>
2021-04-28 01:42:22 +0600

    Add code in HomeFragment

--- app/src/main/java/com/ninjapath/besteducation/fragments/HomeFragment.java
+++ app/src/main/java/com/ninjapath/besteducation/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.ninjapath.besteducation.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,13 +10,21 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.ninjapath.besteducation.R;
+import com.ninjapath.besteducation.activities.CreateActivity;
 
 public class HomeFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View fragmentView = inflater.inflate(R.layout.fragment_home, container, false);
+        FloatingActionButton createButton = fragmentView.findViewById(R.id.create_course_button);
+        createButton.setOnClickListener(view -> {
+            Intent intentToCreateActivity = new Intent(getActivity(), CreateActivity.class);
+            startActivity(intentToCreateActivity);
+        });
+        return fragmentView;
     }
 }
