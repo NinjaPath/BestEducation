da81f94eeaff2666f76835ca9105b9d100f43f81
OlegSadJktu <plyuha2004@mail.ru>
2021-05-07 22:50:27 +0600

    Fixed code fragment in GreetingActivity

--- app/src/main/java/com/ninjapath/besteducation/activities/GreetingActivity.java
+++ app/src/main/java/com/ninjapath/besteducation/activities/GreetingActivity.java
@@ -2,19 +2,30 @@ package com.ninjapath.besteducation.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.ninjapath.besteducation.R;
+import com.ninjapath.besteducation.enums.AccountType;
 
 public class GreetingActivity extends AppCompatActivity {
 
+    private static final String TAG = "GreetingActivity";
     private Button signIn;
     private Button logIn;
     private FirebaseAuth mAuth;
+    FirebaseFirestore fstore ;
+
 
     @Override
     protected void onStart() {
@@ -23,9 +34,23 @@ public class GreetingActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser != null) {
-            Intent intentToHome = new Intent(GreetingActivity.this,
-                    MainActivity.class);
+            fstore = FirebaseFirestore.getInstance();
+            Intent intentToHome = new Intent(GreetingActivity.this, MainActivity.class);
+            intentToHome.putExtra(MainActivity.ACCOUNT_TYPE, AccountType.TEACHER);
             startActivity(intentToHome);
+//            fstore.collection("users").document(mAuth.getCurrentUser().getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//                @Override
+//                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                    if (task.isSuccessful()) {
+//                        DocumentSnapshot document = task.getResult();
+//                        Intent intentToHome = new Intent(GreetingActivity.this, MainActivity.class);
+//                        intentToHome.putExtra(MainActivity.ACCOUNT_TYPE, AccountType.valueOf(
+//                                document.getString("accountType").toUpperCase()));
+//                        startActivity(intentToHome);
+//                    }
+//                }
+//            });
+
         }
     }
 
