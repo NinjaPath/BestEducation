c9210aa8237b0bc60714f9fb050ad1a1cee29286
OlegSadJktu <plyuha2004@mail.ru>
2021-05-08 13:27:56 +0600

    Added cards

--- app/src/main/java/com/ninjapath/besteducation/adapters/TeacherCoursesAdapter.java
+++ app/src/main/java/com/ninjapath/besteducation/adapters/TeacherCoursesAdapter.java
@@ -1,37 +1,56 @@
 package com.ninjapath.besteducation.adapters;
 
+import android.media.Image;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.ninjapath.besteducation.R;
 
 public class TeacherCoursesAdapter extends RecyclerView.Adapter<TeacherCoursesAdapter.ViewHolder> {
 
-
+    private String[] linksToImages;
+    private String[] courseSignature;
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        CardView cv = (CardView) LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.courses_card, parent, false);
+        return new ViewHolder(cv);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        CardView cardView = holder.cardView;
+        ImageView imageView = cardView.findViewById(R.id.image_card);
+        TextView signature = cardView.findViewById(R.id.signature);
+        
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return  linksToImages.length;
     }
 
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        public ViewHolder(@NonNull View itemView) {
+        private CardView cardView;
+
+        public ViewHolder(@NonNull CardView itemView) {
             super(itemView);
+            cardView = itemView;
         }
+
+
+
     }
 
 }
