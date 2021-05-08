d47704af627904c51efb59a38d31a0e94e3617a7
Parkhomenko Egor <54312766+1MPULSEONE@users.noreply.github.com>
2021-05-08 13:29:06 +0600

    HomeFragment changes - Now teacher can see his courses - Updated supported class to work with database - Create new model - Connected Glide dependency - Added new values in colors.xml - Some code refactoring
    Signed-off-by: “Parkhomenko <parkhomenko2049@gmail.com>

--- app/src/main/java/com/ninjapath/besteducation/dao/CreateDao.java
+++ app/src/main/java/com/ninjapath/besteducation/dao/CreateDao.java
@@ -2,9 +2,14 @@ package com.ninjapath.besteducation.dao;
 
 import android.view.View;
 
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.ninjapath.besteducation.CourseData;
 
 public interface CreateDao {
     void loadVideo(View view, CourseData courseData);
+
     void insertData(View view, CourseData courseData);
+
+    void getCoursesRecyclerData(View view, RecyclerView coursesRecycler);
 }
