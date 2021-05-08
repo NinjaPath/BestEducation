20a4cccf29f5f01e654529009cc358193cae99e4
OlegSadJktu <plyuha2004@mail.ru>
2021-05-08 14:53:17 +0600

    Fixed TeacherCoursesAdapter and maked courses_card

--- app/src/main/java/com/ninjapath/besteducation/adapters/TeacherCoursesAdapter.java
+++ app/src/main/java/com/ninjapath/besteducation/adapters/TeacherCoursesAdapter.java
@@ -1,5 +1,6 @@
 package com.ninjapath.besteducation.adapters;
 
+import android.content.Context;
 import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,12 +12,20 @@ import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.ninjapath.besteducation.R;
 
 public class TeacherCoursesAdapter extends RecyclerView.Adapter<TeacherCoursesAdapter.ViewHolder> {
 
     private String[] linksToImages;
     private String[] courseSignature;
+    private Context context;
+
+    public TeacherCoursesAdapter(String[] linksToImages, String[] courseSignature, Context context) {
+        this.linksToImages = linksToImages;
+        this.courseSignature = courseSignature;
+        this.context = context;
+    }
 
     @NonNull
     @Override
@@ -31,7 +40,11 @@ public class TeacherCoursesAdapter extends RecyclerView.Adapter<TeacherCoursesAd
         CardView cardView = holder.cardView;
         ImageView imageView = cardView.findViewById(R.id.image_card);
         TextView signature = cardView.findViewById(R.id.signature);
-        
+        Glide.with(context)
+                .load(linksToImages[position])
+                .into(imageView);
+        imageView.setContentDescription(courseSignature[position]);
+        signature.setText(courseSignature[position]);
     }
 
     @Override
